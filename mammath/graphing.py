6f25bb393580a8442e4749eab8ff7a88d8baf3c8
Harihar Rengan <harihar.rengan@gmail.com>
2024-06-27 19:23:49 -0700

    Update graphing.py

--- mammath/graphing.py
+++ mammath/graphing.py
@@ -8,13 +8,12 @@ from .operations import *
 """
 GRAPHING
 """
-#graph("2*x", "3*x")
-def graph(*args, lrangex=-10, urangex=10, lrangey=-10, urangey=10, graph_points=100, gridset=True, scaling=True, graph_title=None, xtitle=None, ytitle=None):
+def graph2d_func(*args, lrangex=-10, urangex=10, lrangey=-10, urangey=10, graph_points=100, gridset=True, scaling=True, graph_title=None, xtitle=None, ytitle=None):
     """
-    Plots any number of 2D functions given a string function of x equating to y. The first 4 keyword arguments change the axis of the graph.
+    Plots any number of 2D functions given a function of x equating to y. The first 4 keyword arguments change the axis of the graph.
     
     Args:
-        *args (str): Any number of string functions of x equating to y.
+        *args (function): Any number of functions of x equating to y.
         lrangex (float, optional): The lower bound for the x-axis. Defaults to -10.
         urangex (float, optional): The upper bound for the x-axis. Defaults to 10.
         lrangey (float, optional): The lower bound for the y-axis. Defaults to -10.
@@ -29,35 +28,169 @@ def graph(*args, lrangex=-10, urangex=10, lrangey=-10, urangey=10, graph_points=
     Returns:
         None
     """
-    args = list(args)
-    for i in range(0, len(args)):
-        args[i] = parse_graphing(args[i])
     
-    i = 0
     x = np.linspace(lrangex, urangex, graph_points)
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
+    fig, ax = plt.subplots()
+    
     ax.spines["left"].set_position("center")
     ax.spines["bottom"].set_position("zero")
     ax.spines["right"].set_color("none")
     ax.spines["top"].set_color("none")
     ax.xaxis.set_ticks_position("bottom")
     ax.yaxis.set_ticks_position("left")
-    while i < len(args):
-        if type(eval(args[i])) == np.ndarray:
-            y = eval(args[i])
-            plt.plot(x,y, "-b", label=args)
-            i+=1
-    plt.title(graph_title)
-    plt.xlabel(xtitle,fontsize=20)
-    plt.ylabel(ytitle,fontsize=20)
+    
+    for func in args:
+        y = func(x)
+        ax.plot(x, y, label=func.__name__)
+    
+    if graph_title:
+        plt.title(graph_title)
+    if xtitle:
+        plt.xlabel(xtitle, fontsize=20)
+    if ytitle:
+        plt.ylabel(ytitle, fontsize=20)
+        
     plt.autoscale(enable=scaling)
     plt.grid(gridset)
-    if lrangex or urangex != False:
-        plt.xlim([lrangex,urangex])
-        if lrangey or urangey != False:
-            plt.ylim([lrangey,urangey])
-            plt.show()
+    
+    if lrangex is not None and urangex is not None:
+        plt.xlim([lrangex, urangex])
+    if lrangey is not None and urangey is not None:
+        plt.ylim([lrangey, urangey])
+    
+    plt.legend()
+    plt.show()
+
+def graph3d_func(*args, plot_type='contour', lrangex=-10, lrangey=-10, urangex=10, urangey=10, lrangez=-10, urangez=10, graph_points=1000, lablex='x', labley='y', lablez='z', graph_title=None, cmap='binary'):
+    """
+    Plots any number of 3D functions given a function of x and y as a contour, wireframe, surface, or other plot. The first 6 keyword arguments change the axis of the graph.
+    
+    Args:
+        *args (function): Any number of functions of x and y equating to z.
+        plot_type (str, optional): Type of plot - 'contour', 'wireframe', 'surface', etc. Defaults to 'contour'.
+        lrangex (float, optional): The lower bound for the x-axis. Defaults to -10.
+        lrangey (float, optional): The lower bound for the y-axis. Defaults to -10.
+        urangex (float, optional): The upper bound for the x-axis. Defaults to 10.
+        urangey (float, optional): The upper bound for the y-axis. Defaults to 10.
+        lrangez (float, optional): The lower bound for the z-axis. Defaults to -10.
+        urangez (float, optional): The upper bound for the z-axis. Defaults to 10.
+        graph_points (int, optional): The number of points to sample for the graph. Defaults to 1000.
+        lablex (str, optional): The label for the x-axis. Defaults to 'x'.
+        labley (str, optional): The label for the y-axis. Defaults to 'y'.
+        lablez (str, optional): The label for the z-axis. Defaults to 'z'.
+        graph_title (str, optional): The title of the graph. Defaults to None.
+        cmap (str, optional): The colormap to use for the plot. Defaults to 'binary'.
+
+    Returns:
+        None
+    """
+    
+    x = np.linspace(lrangex, urangex, int(np.sqrt(graph_points)))
+    y = np.linspace(lrangey, urangey, int(np.sqrt(graph_points)))
+    X, Y = np.meshgrid(x, y)
+    
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    
+    for func in args:
+        Z = func(X, Y)
+        if plot_type == 'contour':
+            ax.contour3D(X, Y, Z, 50, cmap=cmap)
+        elif plot_type == 'wireframe':
+            ax.plot_wireframe(X, Y, Z)
+        elif plot_type == 'surface':
+            ax.plot_surface(X, Y, Z, cmap=cmap)
+        elif plot_type == 'trisurf':
+            ax.plot_trisurf(X.flatten(), Y.flatten(), Z.flatten(), cmap=cmap)
+        elif plot_type == 'scatter':
+            ax.scatter(X, Y, Z, c=Z, cmap=cmap)
+        else:
+            raise ValueError("plot_type must be 'contour', 'wireframe', 'surface', 'trisurf', or 'scatter'")
+    
+    ax.set_xlabel(lablex)
+    ax.set_ylabel(labley)
+    ax.set_zlabel(lablez)
+    if graph_title:
+        ax.set_title(graph_title)
+    
+    if lrangez is not None and urangez is not None:
+        ax.set_zlim(lrangez, urangez)
+    if lrangex is not None and urangex is not None:
+        ax.set_xlim(lrangex, urangex)
+    if lrangey is not None and urangey is not None:
+        ax.set_ylim(lrangey, urangey)
+    
+    plt.show()
+
+
+def graph2d_parametric(*args, t_range=(0, 2*np.pi), graph_points=1000, lablex='x', labley='y', graph_title=None):
+    """
+    Plots multiple 2D parametric functions.
+    
+    Args:
+        *args (tuple): Tuples of (x_func, y_func) where each is a function of t.
+        t_range (tuple, optional): The range of the parameter t. Defaults to (0, 2*pi).
+        graph_points (int, optional): The number of points to sample for the graph. Defaults to 1000.
+        lablex (str, optional): The label for the x-axis. Defaults to 'x'.
+        labley (str, optional): The label for the y-axis. Defaults to 'y'.
+        graph_title (str, optional): The title of the graph. Defaults to None.
+
+    Returns:
+        None
+    """
+    
+    t = np.linspace(t_range[0], t_range[1], graph_points)
+    
+    for x_func, y_func in args:
+        x = x_func(t)
+        y = y_func(t)
+        plt.plot(x, y, label=f'{x_func.__name__}, {y_func.__name__}')
+    
+    plt.xlabel(lablex)
+    plt.ylabel(labley)
+    if graph_title:
+        plt.title(graph_title)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def graph3d_parametric(*args, t_range=(0, 2*np.pi), graph_points=1000, lablex='x', labley='y', lablez='z', graph_title=None, cmap='viridis'):
+    """
+    Plots multiple 3D parametric functions.
+    
+    Args:
+        *args (tuple): Tuples of (x_func, y_func, z_func) where each is a function of t.
+        t_range (tuple, optional): The range of the parameter t. Defaults to (0, 2*pi).
+        graph_points (int, optional): The number of points to sample for the graph. Defaults to 1000.
+        lablex (str, optional): The label for the x-axis. Defaults to 'x'.
+        labley (str, optional): The label for the y-axis. Defaults to 'y'.
+        lablez (str, optional): The label for the z-axis. Defaults to 'z'.
+        graph_title (str, optional): The title of the graph. Defaults to None.
+        cmap (str, optional): The colormap to use for the plot. Defaults to 'viridis'.
+
+    Returns:
+        None
+    """
+    
+    t = np.linspace(t_range[0], t_range[1], graph_points)
+    
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    
+    for x_func, y_func, z_func in args:
+        X = x_func(t)
+        Y = y_func(t)
+        Z = z_func(t)
+        ax.plot3D(X, Y, Z, label=f'{x_func.__name__}, {y_func.__name__}, {z_func.__name__}')
+    
+    ax.set_xlabel(lablex)
+    ax.set_ylabel(labley)
+    ax.set_zlabel(lablez)
+    if graph_title:
+        ax.set_title(graph_title)
+    ax.legend()
+    
+    plt.show()
 
 def plot_histogram(data, bins=10, range=None, xlabel='Value', ylabel='Frequency', title=None):
     """
@@ -338,61 +471,6 @@ def graph3d_line(*args, lrangex=-10, lrangey=-10, lrangez = -10, urangex=10, ura
         ax.set_zlim(lrangey, urangey)
     return plt.show()
 
-def graph3d_wire(*args, lrangex=-10, lrangey=-10, urangex=10, urangey=10, lrangez=-10, urangez=10, graph_points=1000, lablex='x', labley='y', lablez='z', graph_title=None, edgecolor=False, color='black', fill='viridis'):
-    """
-    Plots any number of 3D wireframe functions given a string function of x equating to y as a wire. The first 6 keyword arguments change the axis of the graph.
-    
-    Args:
-        *args (str): Any number of string functions of x equating to y.
-        lrangex (float, optional): The lower bound for the x-axis. Defaults to -10.
-        lrangey (float, optional): The lower bound for the y-axis. Defaults to -10.
-        urangex (float, optional): The upper bound for the x-axis. Defaults to 10.
-        urangey (float, optional): The upper bound for the y-axis. Defaults to 10.
-        lrangez (float, optional): The lower bound for the z-axis. Defaults to -10.
-        urangez (float, optional): The upper bound for the z-axis. Defaults to 10.
-        graph_points (int, optional): The number of points to sample for the graph. Defaults to 1000.
-        lablex (str, optional): The label for the x-axis. Defaults to 'x'.
-        labley (str, optional): The label for the y-axis. Defaults to 'y'.
-        lablez (str, optional): The label for the z-axis. Defaults to 'z'.
-        graph_title (str, optional): The title of the graph. Defaults to None.
-        edgecolor (bool, optional): Whether to display edge colors for the wireframe or not. Defaults to False.
-        color (str, optional): The color of the wireframe lines. Defaults to 'black'.
-        fill (str, optional): The colormap to use for the wireframe. Defaults to 'viridis'
-        
-    Returns:
-        None
-    """
-
-    x = np.linspace(lrangex, urangex, graph_points)
-    y = np.linspace(lrangey, urangey, graph_points)
-    ax = plt.axes(projection='3d')
-    X, Y = np.meshgrid(x, y)
-    i = 0
-    while i < len(args):
-        Z = f(args[i], X, Y)
-        if fill==False:
-            ax.plot_wireframe(X, Y, Z, color=color)
-        elif edgecolor == True:
-            ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                    cmap=fill, edgecolor=color)
-        elif edgecolor == False:
-            ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                    cmap=fill, edgecolor='none')
-        else:
-            raise TypeError('Parameter "edgecolor" must be a boolean operator')
-        i += 1
-    ax.set_xlabel(lablex)
-    ax.set_ylabel(labley)
-    ax.set_zlabel(lablez)   
-    ax.set_title(graph_title)
-    if lrangez != False or urangez == False:
-        ax.set_zlim(lrangez, urangez)
-    if lrangex != False or urangex == False:
-        ax.set_zlim(lrangex, urangex)
-    if lrangey != False or urangey == False:
-        ax.set_zlim(lrangey, urangey)
-    return plt.show()
-
 def graph3d_scatter(x_data, y_data, z_data, xlabel='x', ylabel='y', zlabel='z', graph_title=None):
     """
     Plots a 3D scatter plot with the given x, y, and z data points.
@@ -446,101 +524,6 @@ def graph3d_bar(x_data, y_data, z_data, xlabel='x', ylabel='y', zlabel='z', grap
     ax.set_title(graph_title)
 
     return plt.show()
-
-def graph3d_surface(fx, lrangex=-10, lrangey=-10, urangex=10, urangey=10, lrangez=-10, urangez=10, graph_points=1000, lablex='x', labley='y', lablez='z', graph_title=None, edgecolor=False, color='black', fill='viridis'):
-    """
-    Plots a 3D surface function given a string function fx of x and y.
-    
-    Args:
-        fx (str): The string function of x and y.
-        lrangex (float, optional): The lower bound for the x-axis. Defaults to -10.
-        lrangey (float, optional): The lower bound for the y-axis. Defaults to -10.
-        urangex (float, optional): The upper bound for the x-axis. Defaults to 10.
-        urangey (float, optional): The upper bound for the y-axis. Defaults to 10.
-        lrangez (float, optional): The lower bound for the z-axis. Defaults to -10.
-        urangez (float, optional): The upper bound for the z-axis. Defaults to 10.
-        graph_points (int, optional): The number of points to sample for the graph. Defaults to 1000.
-        lablex (str, optional): The label for the x-axis. Defaults to 'x'.
-        labley (str, optional): The label for the y-axis. Defaults to 'y'.
-        lablez (str, optional): The label for the z-axis. Defaults to 'z'.
-        graph_title (str, optional): The title of the graph. Defaults to None.
-        edgecolor (bool, optional): Whether to display edge colors for the surface or not. Defaults to False.
-        color (str, optional): The color of the surface lines. Defaults to 'black'.
-        fill (str, optional): The colormap to use for the surface. Defaults to 'viridis'
-        
-    Returns:
-        None
-    """
-    
-    x = np.linspace(lrangex, urangex, graph_points)
-    y = np.linspace(lrangey, urangey, graph_points)
-    X, Y = np.meshgrid(x, y)
-    Z = f(fx, X, Y)
-    
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    
-    if edgecolor == True:
-        ax.plot_surface(X, Y, Z, cmap=fill, edgecolor=color)
-    elif edgecolor == False:
-        ax.plot_surface(X, Y, Z, cmap=fill, edgecolor='none')
-    else:
-        raise TypeError('Parameter "edgecolor" must be a boolean operator')
-    
-    ax.set_xlabel(lablex)
-    ax.set_ylabel(labley)
-    ax.set_zlabel(lablez)
-    ax.set_title(graph_title)
-    
-    ax.set_zlim(lrangez, urangez)
-    ax.set_xlim(lrangex, urangex)
-    ax.set_ylim(lrangey, urangey)
-    
-    plt.show()
-
-def graph3d_wireframe(fx, lrangex=-10, lrangey=-10, urangex=10, urangey=10, lrangez=-10, urangez=10, graph_points=1000, lablex='x', labley='y', lablez='z', graph_title=None, color='black'):
-    """
-    Plots a 3D wireframe function given a string function fx of x and y.
-    
-    Args:
-        fx (str): The string function of x and y.
-        lrangex (float, optional): The lower bound for the x-axis. Defaults to -10.
-        lrangey (float, optional): The lower bound for the y-axis. Defaults to -10.
-        urangex (float, optional): The upper bound for the x-axis. Defaults to 10.
-        urangey (float, optional): The upper bound for the y-axis. Defaults to 10.
-        lrangez (float, optional): The lower bound for the z-axis. Defaults to -10.
-        urangez (float, optional): The upper bound for the z-axis. Defaults to 10.
-        graph_points (int, optional): The number of points to sample for the graph. Defaults to 1000.
-        lablex (str, optional): The label for the x-axis. Defaults to 'x'.
-        labley (str, optional): The label for the y-axis. Defaults to 'y'.
-        lablez (str, optional): The label for the z-axis. Defaults to 'z'.
-        graph_title (str, optional): The title of the graph. Defaults to None.
-        color (str, optional): The color of the wireframe lines. Defaults to 'black'.
-        
-    Returns:
-        None
-    """
-    
-    x = np.linspace(lrangex, urangex, graph_points)
-    y = np.linspace(lrangey, urangey, graph_points)
-    X, Y = np.meshgrid(x, y)
-    Z = f(fx, X, Y)
-    
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    
-    ax.plot_wireframe(X, Y, Z, color=color)
-    
-    ax.set_xlabel(lablex)
-    ax.set_ylabel(labley)
-    ax.set_zlabel(lablez)
-    ax.set_title(graph_title)
-    
-    ax.set_zlim(lrangez, urangez)
-    ax.set_xlim(lrangex, urangex)
-    ax.set_ylim(lrangey, urangey)
-    
-    plt.show()
     
 """
 END OF GRAPHING
